2e8ca17b2ef7e954e386454296863bb15c19596d
wahwahmayphu <wahwahmayphu1997@gmail.com>
2025-09-09 22:01:05 +0630

    functions to upload files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FileService } from './file.service';
+import { HttpErrorResponse, HttpEvent } from '@angular/common/http';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,44 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'uploadanddownload';
+
+  constructor (private fileservice : FileService){}
+
+  //define a function to upload files
+  onUploadFiles(files: File[]) : void {
+    const formData = new FormData();
+    for(const file of files) { 
+      formData.append('files', file, file.name); 
+    }
+    this.fileservice.upload(formData).subscribe(
+      event => {
+        console.log(event);
+        this.resportProgres(event)
+      },
+      (error: HttpErrorResponse) => { 
+        console.log(error) 
+      }
+    );
+  }
+
+  //define a function to download files
+  onDownloadFiles(files: File[]) : void {
+    const formData = new FormData();
+    for(const file of files) { 
+      formData.append('files', file, file.name); 
+    }
+    this.fileservice.upload(formData).subscribe(
+      event => {
+        console.log(event);
+        this.resportProgres(event)
+      },
+      (error: HttpErrorResponse) => { 
+        console.log(error) 
+      }
+    );
+  }
+
+  private resportProgres(event: HttpEvent<string[]>) {
+    throw new Error('Method not implemented.');
+  }
 }
